f046d7d50235d042ba0815c8cfa30f47aeb7d4aa
Vish0111 <it21315282@my.sliit.lk>
2023-08-24 23:51:15 +0530

    Data Import Done

---
+++ backend/data/products.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+      name: "Combo Pack 1",
+      imageUrl:
+        "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+      description:
+        "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
+      price: 499,
+      countInStock: 15,
+    },
+    {
+      name: "Combo Pack 2",
+      imageUrl:
+        "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+      description:
+        "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
+      price: 1099,
+      countInStock: 10,
+    }, 
+    
+  ];
+  
+  module.exports = products;
+  
