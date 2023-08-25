985631a7177b731915925ec291addde8e74c907c
Vish0111 <it21315282@my.sliit.lk>
2023-08-25 11:10:36 +0530

    Product Screen Completed

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -3,15 +3,17 @@ import Product from "../components/Product";
 
 const Home = () => {
     return (
-        <div>
-        <h1>Deals And Exclusives</h1>
+        <div className="homescreen">
+        <h1 className="homescreen__title">Deals And Exclusives</h1>
         <h2>Get the best deals and exclusives on your favorite movies</h2>
         <h3>Shop Now</h3>
+        <div className="homescreen__products">
         <Product/>
         <Product/>
         <Product/>
         <Product/>
         <Product/>
+        </div>
         </div>
     );
 };
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,40 @@
 import './ProductScreen.css'
 
 const ProductScreen = () => {
-    return (<div className="productscreen"> Product Screen  </div>);
+    return (<div className="productscreen"> 
+    <div className="productscreen__left">
+    <div className="left__image">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXmmvCUi1_F5O5jtHQr6_9BfAF1i7iRiqr2MSrzToqC5hb6_WoUPVyxphd5-tLRrjQJAE&usqp=CAU" alt="product name"/>
+    </div>
+    <div className="left__info">
+    <p className="left__name">Package1</p>
+    <p>Price: Rs500</p>
+    <p>Description: Pop corn + pepsi ...</p>
+        </div>
+    </div>
+    <div className="productscreen__right">
+    <div className="right__info">
+    <p>
+    Price: <span>Rs500</span>
+    </p>
+    <p>
+    Status: <span>In Stock</span>
+    </p>
+    <p>
+    Qty
+    <select>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    </select>
+    </p>
+    <p>
+    <button type="button">Add To Cart</button>
+    </p>
+        </div>
+    </div>
+     </div>);
     
 
 };
