80540aad7690add9417c8cc86ca37a4f5ffcdaf0
Vish0111 <it21315282@my.sliit.lk>
2023-08-25 16:29:56 +0530

    Retrive data from db done

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,22 +1,23 @@
 const products = [
     {
-      name: "Combo Pack 1",
+      name: "Combo concession Pack 1",
       imageUrl:
-        "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYzOTjGhvPUIvzt_ItSRyjLmt_TarDy6W17w&usqp=CAU",
       description:
-        "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
-      price: 499,
+        "Special deal for movie lovers One Large Cheese Popcorn and One Medium Drink.",
+      price: 400,
       countInStock: 15,
     },
     {
-      name: "Combo Pack 2",
+      name: "Combo concession Pack 2",
       imageUrl:
-        "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpUJojmjAeT1T0PczNPd-X0o1cbUg4RHImtQ&usqp=CAU",
       description:
-        "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
-      price: 1099,
+        "SSpecial deal for couples One Large Popcorn and Two Drinks (Medium).",
+      price: 900,
       countInStock: 10,
-    }, 
+    },
+    
     
   ];
   
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,19 @@
 import './Product.css';
 import { Link } from "react-router-dom";
 
-const Product = () => {
+const Product = ({imageUrl, name, price, description, productId}) => {
   return (
     <div className="product">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXmmvCUi1_F5O5jtHQr6_9BfAF1i7iRiqr2MSrzToqC5hb6_WoUPVyxphd5-tLRrjQJAE&usqp=CAU" alt="product 1" />
+      <img src={imageUrl} alt={name}/>
 
       <div className="product__info">
-        <p className="info__name">Package1</p>
+        <p className="info__name">{name}</p>
 
-        <p className="info__description">Pop corn + pepsi ...</p>
+        <p className="info__description">{description.substring(0, 100)}...</p>
 
-        <p className="info__price">Rs 500</p>
+        <p className="info__price">Rs{price}</p>
 
-        <Link to={`/product/${1111}`} className="info__button">
+        <Link to={`/product/${productId}`} className="info__button">
           View
         </Link>
       </div>
