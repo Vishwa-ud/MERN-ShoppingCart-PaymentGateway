81606000c07925062a02cd012af1907a34dbc916
Vish0111 <it21315282@my.sliit.lk>
2023-08-25 12:44:39 +0530

    redux store add

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
-root.render(
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//root.render(
+  ReactDOM.render(
+  <Provider 
+  store={store}>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
