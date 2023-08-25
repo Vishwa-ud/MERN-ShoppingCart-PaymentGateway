539184a826e087f3ae7b91dc42e3aad21d3c2c67
Vish0111 <it21315282@my.sliit.lk>
2023-08-25 10:46:15 +0530

    Products add to home Screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,19 @@
-import "./Product.css";
+import './Product.css';
 import { Link } from "react-router-dom";
 
-const Product = ({ imageUrl, description, price, name, productId }) => {
+const Product = () => {
   return (
     <div className="product">
-      <img src={imageUrl} alt={name} />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXmmvCUi1_F5O5jtHQr6_9BfAF1i7iRiqr2MSrzToqC5hb6_WoUPVyxphd5-tLRrjQJAE&usqp=CAU" alt="product 1" />
 
       <div className="product__info">
-        <p className="info__name">{name}</p>
+        <p className="info__name">Package1</p>
 
-        <p className="info__description">{description.substring(0, 100)}...</p>
+        <p className="info__description">Pop corn + pepsi ...</p>
 
-        <p className="info__price">${price}</p>
+        <p className="info__price">Rs 500</p>
 
-        <Link to={`/product/${productId}`} className="info__button">
+        <Link to={`/product/${1111}`} className="info__button">
           View
         </Link>
       </div>
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,13 +1,17 @@
+import './Home.css'
+import Product from "../components/Product";
+
 const Home = () => {
     return (
         <div>
         <h1>Deals And Exclusives</h1>
         <h2>Get the best deals and exclusives on your favorite movies</h2>
         <h3>Shop Now</h3>
-        <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXmmvCUi1_F5O5jtHQr6_9BfAF1i7iRiqr2MSrzToqC5hb6_WoUPVyxphd5-tLRrjQJAE&usqp=CAU"
-        alt="car"
-      />
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
         </div>
     );
 };
