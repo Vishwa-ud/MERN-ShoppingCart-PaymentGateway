6ac743a71a38a4997172daa1b00fa0641dd572c2
Vish0111 <it21315282@my.sliit.lk>
2023-08-25 13:01:52 +0530

    did some changes

--- frontend/src/redux/constants/cartConstants.js
+++ frontend/src/redux/constants/cartConstants.js
@@ -1,4 +1,4 @@
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
-export const CART_RESET = 'CART_RESET';
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const CART_RESET = "CART_RESET";
 
