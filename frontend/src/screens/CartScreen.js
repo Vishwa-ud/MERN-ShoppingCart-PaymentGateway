f30bd7d4b1e34c587903e7ff36c6b476e59846f4
Vish0111 <it21315282@my.sliit.lk>
2023-10-06 01:02:18 +0530

    added both gateways stripe and new gateway

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import './CartScreen.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { loadStripe } from '@stripe/stripe-js';
 
 // Components
@@ -14,6 +14,9 @@ const CartScreen = () => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const navigate = useNavigate();
+
+  const [selectedGateway, setSelectedGateway] = useState('stripe');
 
   const qtyChangeHandler = (id, qty) => {
     dispatch(addToCart(id, qty));
@@ -32,41 +35,45 @@ const CartScreen = () => {
   };
 
   const makePayment = async () => {
-    const stripe = await loadStripe(
-      'pk_test_51Ns9obAuazamskfxFgZzz5Z9X2tDM9NDLLI0Wserb178ONKNbJ8hnbb7a9AqqCgEd0PTJDxKKYgUZDwnMy6skPcM00LieKKLAa' // Replace with your actual Stripe public key
-    );
+    if (selectedGateway === 'stripe') {
+      // Stripe payment logic here
+      const stripe = await loadStripe('pk_test_51Ns9obAuazamskfxFgZzz5Z9X2tDM9NDLLI0Wserb178ONKNbJ8hnbb7a9AqqCgEd0PTJDxKKYgUZDwnMy6skPcM00LieKKLAa');
+      
+      const body = {
+        products: cartItems,
+      };
 
-    const body = {
-      products: cartItems,
-    };
+      const headers = {
+        'Content-Type': 'application/json',
+      };
 
-    const headers = {
-      'Content-Type': 'application/json',
-    };
-
-    try {
-      const response = await fetch('/api/create-checkout-session', {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(body),
-      });
-
-      if (response.ok) {
-        const session = await response.json();
-
-        const result = await stripe.redirectToCheckout({
-          sessionId: session.id,
+      try {
+        const response = await fetch('/api/create-checkout-session', {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify(body),
         });
 
-        if (result.error) {
-          alert(result.error.message);
+        if (response.ok) {
+          const session = await response.json();
+
+          const result = await stripe.redirectToCheckout({
+            sessionId: session.id,
+          });
+
+          if (result.error) {
+            alert(result.error.message);
+          }
+        } else {
+          throw new Error('Failed to create checkout session');
         }
-      } else {
-        throw new Error('Failed to create checkout session');
+      } catch (error) {
+        console.error(error);
+        alert('An error occurred while processing your payment.');
       }
-    } catch (error) {
-      console.error(error);
-      alert('An error occurred while processing your payment.');
+    } else {
+      // Navigate to the custom payment gateway route
+      navigate('/payment'); // Update the route as needed
     }
   };
 
@@ -95,6 +102,15 @@ const CartScreen = () => {
           <p>Rs.{getCartSubTotal().toFixed(2)}</p>
         </div>
         <div>
+          <label htmlFor="paymentGateway">Select Payment Gateway:</label>
+          <select
+            id="paymentGateway"
+            value={selectedGateway}
+            onChange={(e) => setSelectedGateway(e.target.value)}
+          >
+            <option value="stripe">Pay with Stripe</option>
+            <option value="custom">Pay with Custom Gateway</option>
+          </select>
           <button onClick={makePayment}>Proceed To Checkout</button>
         </div>
       </div>
